e219912c47e8d4f1e4b2ada3874888eed0a7bf01
winebarrel <sugawara@winebarrel.jp>
2023-01-15 11:00:30 +0900

    fix for eslint

--- src/install.ts
+++ src/install.ts
@@ -1,16 +1,16 @@
-import * as core from "@actions/core";
-import * as tc from "@actions/tool-cache";
-import path from "path";
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import path from 'path'
 
-const downloadURL = "https://github.com/winebarrel/pru/releases/download";
+const downloadURL = 'https://github.com/winebarrel/pru/releases/download'
 
 export async function install(version: string): Promise<string> {
-  core.info(`Installing pru ${version} ...`);
-  const assetURL = `${downloadURL}/v${version}/pru_${version}_linux_amd64.tar.gz`;
-  core.info(`Downloading ${assetURL} ...`);
-  const archivePath = await tc.downloadTool(assetURL);
+  core.info(`Installing pru ${version} ...`)
+  const assetURL = `${downloadURL}/v${version}/pru_${version}_linux_amd64.tar.gz`
+  core.info(`Downloading ${assetURL} ...`)
+  const archivePath = await tc.downloadTool(assetURL)
   const extractedDir = await tc.extractTar(archivePath, process.env.HOME, [
-    "xz",
-  ]);
-  return path.join(extractedDir, `pru`);
+    'xz'
+  ])
+  return path.join(extractedDir, `pru`)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,45 @@
-import * as core from "@actions/core";
-import { install } from "./install";
-import { exec } from "child_process";
-import { promisify } from "util";
-const execShellCommand = promisify(exec);
+import * as core from '@actions/core'
+import {install} from './install'
+import {exec} from 'child_process'
+import {promisify} from 'util'
+const execShellCommand = promisify(exec)
 
 async function run(): Promise<void> {
   try {
-    const version = core.getInput("version", { required: true });
-    const pruPath = await install(version);
-    const repo = core.getInput("repo", { required: true });
+    const version = core.getInput('version', {required: true})
+    const pruPath = await install(version)
+    const repo = core.getInput('repo', {required: true})
     const paths = core
-      .getInput("paths", { required: true })
+      .getInput('paths', {required: true})
       .split(/\s+/)
-      .map((p) => p.trim());
-    const token = core.getInput("github-token", { required: true });
+      .map(p => p.trim())
+    const token = core.getInput('github-token', {required: true})
 
-    core.info(`Running pru in ${repo} ...`);
+    core.info(`Running pru in ${repo} ...`)
 
     const res = await execShellCommand(
-      `${pruPath} ${repo} '${paths.join("' '")}'`
-    );
+      `${pruPath} ${repo} '${paths.join("' '")}'`,
+      {
+        env: {
+          ...process.env,
+          GITHUB_TOKEN: token
+        }
+      }
+    )
 
-    core.info(res.stdout);
-    core.info("Ran pru");
+    if (res.stdout) {
+      core.info(res.stdout)
+    }
+
+    core.info('Ran pru')
   } catch (error) {
-    core.error(`Failed to run: ${error}, ${error.stack}`);
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.error(`Failed to run: ${error}, ${error.stack}`)
+      core.setFailed(error.message)
+    } else {
+      core.error(`Failed to run: ${error}`)
+    }
   }
 }
 
-run();
+run()
